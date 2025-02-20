8ff3190ebc190986ab751168da8d0f1de26b18ee
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2025-02-20 18:05:42 +0530

    ci: add delay to prevent http 429

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import shutil
+import time
 from pathlib import Path
 
 import nox
@@ -9,6 +10,9 @@ import nox
 nox.needs_version = ">=2024.10.9"
 nox.options.default_venv_backend = "uv"
 nox.options.sessions = ["lint", "build-docs"]
+
+
+CI = bool(os.getenv("CI"))
 
 
 def install(session: nox.Session) -> None:
@@ -58,9 +62,17 @@ def build_docs(session: nox.Session) -> None:
     src = Path.cwd() / "docs" / "source"
     build = Path.cwd() / "docs" / "build"
 
+    def sleep() -> None:
+        if CI:
+            time.sleep(30)
+
     session.run("sdr", "unmuxed", "--outfile", src / "unmuxed.md", *session.posargs)
+    sleep()
     session.run("sdr", "no-comparisons", "--outfile", src / "no-comparisons.md", *session.posargs)
+    sleep()
     session.run("sdr", "marked-incomplete", "--outfile", src / "marked-incomplete.md", *session.posargs)
+    sleep()
     session.run("sdr", "public-non-nyaa", "--outfile", src / "public-non-nyaa.md", *session.posargs)
+    sleep()
     session.run("sdr", "private-tracker-only", "--outfile", src / "private-tracker-only.md", *session.posargs)
     session.run("sphinx-build", "-M", "html", src, build)
