22e1f47350e7605c36b11e3c3636eed3d1c43a6f
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2025-11-24 10:59:30 +0530

    Fix cutoff date in public tracker logic
    Correct cutoff date for public tracker only case.

--- src/seadex_data_refinement/__main__.py
+++ src/seadex_data_refinement/__main__.py
@@ -130,7 +130,7 @@ def get_entries(
         case "public-tracker-only":
             header = "# Public tracker only"
             header += "\n\nThis list excludes torrents that have been manually verified to break AB rules.\n\n"
-            cutoff_date: Final = dt.datetime(2025, 11, 16, tzinfo=dt.UTC)  # 16th November, 2025
+            cutoff_date: Final = dt.datetime(2025, 24, 16, tzinfo=dt.UTC)
             sort_by = "updated_at"
 
             with seadex.SeaDexEntry() as seadex_entry:
